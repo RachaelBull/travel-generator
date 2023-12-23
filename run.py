765cb89c5081e3b57f8f16df188e35c174e0488a
RachaelBull <>
2023-12-23 18:26:08 +0000

    First function passed through the CI python validator

--- run.py
+++ run.py
@@ -3,21 +3,27 @@ import time
 import os
 import random
 
+
 def clear_terminal():
     """
     Function to clear the terminal after some inputs
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def introduction_message():
     """
-    Displays an introduction message to the user to indicate what this application is for
+    Displays an introduction message to the user to indicate what this
+    application is for
     """
     clear_terminal()
     print("Welcome to this destination suggestions application.\n")
-    print("You will be asked multiple general questions about your destination preferences which you will answer yes or no.\n")
-    print("After all questions have been answered you will then be displayed with a destination recommendation tailored to your preferences.\n")
-    print("An optional prices average for the month of travel can also be displayed after this conclusion.")
+    print("You will be asked multiple general questions about your destination"
+          " preferences which you will answer yes or no.\n")
+    print("After all questions have been answered you will then be displayed "
+          "with a destination recommendation tailored to your preferences.\n")
+    print("An optional prices average for the month of travel can also be "
+          "displayed after this conclusion.")
 
     # Username validation to only allow letters
     while True:
@@ -26,9 +32,11 @@ def introduction_message():
             print('Invalid name, please try again.\n')
         else:
             clear_terminal()
-            print(f"Hello {user_name}, please take time to think about your preferences to the questions,")
-            print("as your destination generation will depend heavily on this.\n \n")
+            print(f"Hello {user_name}, please take time to think about your "
+                  "preferences to the questions,")
+            print(" as your destination generation will depend on this.\n \n")
             break
+
 
 
 def user_questions():
